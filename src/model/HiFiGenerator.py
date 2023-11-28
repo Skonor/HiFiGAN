9e3c5dad7c3fc128310896a58128637bcb1a7b34
Skonor <gorbunovmikh73@gmail.com>
2023-11-28 18:07:22 +0300

    add weight norm everywhere cause idc

--- src/model/HiFiGenerator.py
+++ src/model/HiFiGenerator.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils import weight_norm
 
 from src.base import BaseModel
 
@@ -14,8 +15,8 @@ class ResBlock(nn.Module):
             Dm_block = []
             for Dnm in Dm:
                 Dm_block.append(nn.LeakyReLU())
-                Dm_block.append(nn.Conv1d(channels, channels, kernel_size=k, dilation=Dnm, 
-                                              padding='same'))
+                Dm_block.append(weight_norm(nn.Conv1d(channels, channels, kernel_size=k, dilation=Dnm, 
+                                              padding='same')))
             self.res_layers.append(nn.Sequential(*Dm_block))
     
     def forward(self, x):
@@ -43,19 +44,19 @@ class HiFiGenerator(BaseModel):
     def __init__(self, ku, kr, Dr, hu=512, num_mels=80):
         super().__init__()
 
-        self.start_conv = nn.Conv1d(num_mels, hu, kernel_size=7, padding='same')
+        self.start_conv = weight_norm(nn.Conv1d(num_mels, hu, kernel_size=7, padding='same'))
         self.body = []
         channels = hu
         for ku_l in ku:
             self.body.append(nn.LeakyReLU())
-            self.body.append(nn.ConvTranspose1d(channels, channels // 2,
-                                                    kernel_size=ku_l, stride=ku_l // 2,
-                                                    padding = ku_l // 4))
+            self.body.append(weight_norm(nn.ConvTranspose1d(channels, channels // 2,
+                                                            kernel_size=ku_l, stride=ku_l // 2,
+                                                            padding = ku_l // 4)))
             self.body.append(MultiReceptieveFieldFusion(kr, Dr, channels // 2))
             channels = channels // 2
 
         self.body = nn.Sequential(*self.body)
-        self.end_conv = nn.Conv1d(channels, 1, kernel_size=7, padding='same')
+        self.end_conv = weight_norm(nn.Conv1d(channels, 1, kernel_size=7, padding='same'))
 
 
     def forward(self, spectrogram, **batch):
